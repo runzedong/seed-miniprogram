e9e13c1d45d38eec530295a5f9444af84af411fc
JizhouZhang <rectcream@gmail.com>
2020-06-13 10:00:12 -0700

    render icon and button on index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,17 @@
 //index.js
 //获取应用实例
+import * as vegeInfo from "../vegeblesInfo"
+
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    vegeList: []
   },
+  
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
@@ -16,11 +19,16 @@ Page({
     })
   },
   onLoad: function () {
+    this.setData({
+      vegeList: vegeInfo.vegetableList
+    })
+
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
       })
+      
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
---
+++ pages/vegeblesInfo.js
@@ -0,0 +1,17 @@
+export const vegetableList = [
+  {
+    "name": "黄瓜",
+    "page": "/pages/detail",
+    "icon": "../resources/cucumber_icon.JPG"
+  },
+  {
+    "name": "西红柿",
+    "page": "/pages/detail",
+    "icon": "../resources/tomato_icon.JPG"
+  },
+  {
+    "name": "茄子",
+    "page": "/pages/detail",
+    "icon": "../resources/eggplant_icon.JPG"
+  }
+]
