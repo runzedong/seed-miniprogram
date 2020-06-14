e0f5304a6562da24bc54d4b8213a186007b538da
Runze <sendrunzedong@gmail.com>
2020-06-13 22:13:05 -0400

    Start entry component

---
+++ pages/components/entry/entry.js
@@ -0,0 +1,23 @@
+Component({
+  properties: {
+    // 这里定义了innerText属性，属性值可以在组件使用时指定
+    entryText: {
+      type: String,
+      value: 'default text',
+    },
+    entryIcon: {
+      type: String,
+      value: 'default icon path',
+    }
+  },
+  data: {
+    // 这里是一些组件内部数据
+    someData: 1
+  },
+  methods: {
+    // 这里是一个自定义方法
+    customMethod: function () {
+      console.log('customMethod')
+    }
+  }
+})
